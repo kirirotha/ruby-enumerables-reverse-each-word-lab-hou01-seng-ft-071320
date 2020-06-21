8d92b4aa8e7fb38032430f8dd544e7bf16337301
Kiri Um <kirirotha@gmail.com>
2020-06-21 14:46:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,14 @@
 def reverse_each_word(string_in)
   word_array = string_in.split(" ")
-  puts word_array
+#  puts word_array
+  main_counter = 0
+  while main_counter < word_array.length do
+      letter_counter=0
+      countdown = word_array[main_counter].length - 1
+      while letter_counter < 
+    
+    main_counter += 1
+  end
 end
 
-reverse_each_word("Hi, how are you?")
+#reverse_each_word("Hi, how are you?")
